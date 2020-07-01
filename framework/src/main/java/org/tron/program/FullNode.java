a890b18dab4cd6bcbab8f800180aad35058f9e16
shenchuanqiang <bill.shen@tron.network>
2020-07-01 18:12:24 +0800

    for test.

--- framework/src/main/java/org/tron/program/FullNode.java
+++ framework/src/main/java/org/tron/program/FullNode.java
@@ -81,10 +81,10 @@ public class FullNode {
     System.out.println(" >>>>>>>>>>> start");
 //    String dbPath = "/Users/tron/Downloads/output-directory";
     String dbPath = "/data/mainnetdb/output-directory";
-//    args = new String[] {"-d", dbPath};
-////    String conf = "/Users/tron/Downloads/main_net_config.conf";
+    args = new String[] {"-d", dbPath};
+//    String conf = "/Users/tron/Downloads/main_net_config.conf";
     String conf = "/home/java-tron/main_net_config.conf";
-    Args.setParam(args, null);
+    Args.setParam(args, conf);
     CommonParameter parameter = Args.getInstance();
 
     load(parameter.getLogbackPath());
